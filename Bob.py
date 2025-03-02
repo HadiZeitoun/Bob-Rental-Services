48f9772c419ae1d55e60aabf019f25aec8a68709
Hadi Zeitoun <Hadii_Zeitoun@hotmail.com>
2025-03-02 16:25:59 +0200

    create class vehicle and adding attributes: brand, model, year, rental_price_per_day

---
+++ Bob.py
@@ -0,0 +1,14 @@
+#creation project structure"
+#create class vehicle
+class vehicle:
+    def __init__(self,brand,model,year,rental_price_per_day):
+        self.brand=brand
+        self.model=model
+        self.year=year
+        self.__rental_price_per_day=rental_price_per_day
+
+
+
+    
+
+        
