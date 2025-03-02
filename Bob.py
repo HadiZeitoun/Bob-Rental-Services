672b36ba9162619695facf94096fd683f574f6ac
Hadi Zeitoun <Hadii_Zeitoun@hotmail.com>
2025-03-02 16:34:49 +0200

    create method get price for class vehicle

--- Bob.py
+++ Bob.py
@@ -7,17 +7,22 @@ class vehicle:
         self.year=year
         self.__rental_price_per_day=rental_price_per_day
 
-def display_info()
+def display_info(self):
     return f"{self.brand} {self.model} ({self.year}) - ${self.__rental_price_per_day}/day"
 
 
 def calculate_cost(self,days):
-    return days*self.__rental_price_per_day 
+    return days*self.__rental_price_per_day
+
+def get_price(self):
+    return self.__rental_price_per_day 
 
 class car(vehicle):
     def __init__(self,brand,model,year,rental_price_per_day,seating_capacity):
         super().__init__(brand,model,year,rental_price_per_day)
         self.seating_capacity=seating_capacity
+        
+
 
     
 
